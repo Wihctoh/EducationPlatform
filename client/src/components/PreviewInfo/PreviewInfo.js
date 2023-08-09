24129388ee6f05a3f760fc12056e78051988b466
Wihctoh <rivertoxi911@gmail.com>
2023-08-09 15:11:18 +0300

    fix css

--- client/src/components/PreviewInfo/PreviewInfo.js
+++ client/src/components/PreviewInfo/PreviewInfo.js
@@ -6,7 +6,7 @@ function PreviewInfo() {
     <>
       <div className={style.preview}>
         <div className={style.previewInfo}>
-          <p className={style.discription}>E-COURSE PLATFORM</p>
+          <p className={style.description}>E-COURSE PLATFORM</p>
           <h1>Learning and teaching online, made easy.</h1>
           <p className={style.info}>
             Any subject, in any language, on any device, for all ages!
@@ -31,25 +31,25 @@ function PreviewInfo() {
         <div className={style.learnLang}>
           <div className={style.learnLangImg}></div>
 
-          <div className={style.learnLangDiscr}>
+          <div className={style.learnLangDescription}>
             <h1>Learn a language in a playful way</h1>
             <p>Make learning programming languages more fun with mini-games</p>
 
-            <div className={style.learnLangDiscrImg}>
-              <div className={style.learnLangDiscrImgSprint}></div>
-              <div className={style.learnLangDiscrImgTasks}></div>
+            <div className={style.learnLangDescriptionImg}>
+              <div className={style.learnLangDescriptionImgSprint}></div>
+              <div className={style.learnLangDescriptionImgTasks}></div>
             </div>
           </div>
         </div>
       </div>
 
       <div className={style.knowledge}>
-        <div className={style.knowledgeDiscription}>
+        <div className={style.knowledgeDescription}>
           <h1>Increase your knowledge</h1>
           <p>Traditional and new effective approaches to learning languages</p>
 
           <Link to={"#!"}>
-            <div className={style.knowledgeDiscriptionBtn}>Textbook →</div>
+            <div className={style.knowledgeDescriptionBtn}>Textbook →</div>
           </Link>
         </div>
 
@@ -60,12 +60,12 @@ function PreviewInfo() {
         <div className={style.watchProgress}>
           <div className={style.watchProgressImg}></div>
 
-          <div className={style.watchProgressDiscription}>
+          <div className={style.watchProgressDescription}>
             <h1>Watch your progress every day</h1>
             <p>Save statistics on your achievements and mistakes</p>
 
             <Link to={"#!"}>
-              <div className={style.watchProgressDiscriptionBtn}>
+              <div className={style.watchProgressDescriptionBtn}>
                 Statistics →
               </div>
             </Link>
