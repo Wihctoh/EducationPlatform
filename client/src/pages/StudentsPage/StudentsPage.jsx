e2e27ff7dfa5b632226c4c282a0d60f68b1bac17
wihctoh <rivertoxi911@gmail.com>
2023-08-25 11:07:40 +0300

    add pagination

--- client/src/pages/StudentsPage/StudentsPage.jsx
+++ client/src/pages/StudentsPage/StudentsPage.jsx
@@ -3,19 +3,25 @@ import style from "./Students.module.css";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+
 const StudentsPage = () => {
   const coursesArr = [
     {
       h2: "JavaScript",
       p: "JavaScript is a practical course where students learn the basics of JavaScript. It covers variables, operators, conditionals, loops, functions, and data manipulation.",
+      img: "./assets/JavaScriptCourseImg.jpeg",
     },
     {
       h2: "TypeScript",
       p: "TypeScript is a course that provides an introduction to TypeScript. Students will learn about TypeScript's key features, such as type annotations, interfaces, classes, and modules",
+      img: "./assets/TypeScriptCourseImg.png",
     },
     {
       h2: "Python",
       p: "Students will learn about variables, data types, conditionals, loops, functions, and file handling. Through hands-on exercises and projects, students will gain proficiency in writing Python code and solving real-world problems.",
+      img: "background:'url(./assets/PythonCourseImg.png)",
     },
   ];
   return (
@@ -41,6 +47,10 @@ const StudentsPage = () => {
           </div>
         ))}
       </div>
+
+      <Stack spacing={2} className={style.pagination}>
+        <Pagination count={3} color="secondary" variant="outlined" />
+      </Stack>
 
       <Footer />
     </>
